bc50a2016ad20a2eddbb1805d99bc0ecf7039e10
james-fairclough <j_fairclough@ymail.com>
2024-12-08 20:35:23 +1100

    testing cloudrun

--- main.py
+++ main.py
@@ -105,5 +105,5 @@ def run():
   return "Hello World!", 200
 
 
-#if __name__ == "__main__":
-#    app.run(debug=True, host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
