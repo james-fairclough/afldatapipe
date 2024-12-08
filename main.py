0fc401df13d3c1e510b652a8cfc05dc33f45bd98
james-fairclough <j_fairclough@ymail.com>
2024-12-08 11:45:04 +1100

    Fixed no response issue

--- main.py
+++ main.py
@@ -101,4 +101,7 @@ def run():
   players = getPlayers(secret)  
   replaceTable('Players',players)
   print(players)
-  print("Done")
+  return "Task completed successfully!", 200
+
+#if __name__ == "__main__":
+#    app.run(debug=True, host="0.0.0.0", port=8080)
