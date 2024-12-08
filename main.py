57d2fd9bef327359a9d789d46c578d90ce10b260
james-fairclough <j_fairclough@ymail.com>
2024-12-08 16:53:08 +1100

    Flask listening to port 8080

--- main.py
+++ main.py
@@ -103,5 +103,7 @@ def run():
   print(players)
   return "Task completed successfully!", 200
 
-#if __name__ == "__main__":
-#    app.run(debug=True, host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+# Use the PORT environment variable
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
