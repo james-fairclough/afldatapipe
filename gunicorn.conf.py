aa389d17190e9258e539c7da605c3d70facf67bd
james-fairclough <j_fairclough@ymail.com>
2024-12-08 16:42:01 +1100

    adding proc file

--- gunicorn.conf.py
+++
@@ -1,6 +0,0 @@
-# Example: gunicorn.conf.py
-bind = "0.0.0.0:8080"  # Bind to all network interfaces on port 8080
-workers = 2            # Set an appropriate number of workers
-threads = 4            # Use threads for concurrency if needed
-preload_app = True      # Preload the application
-timeout = 120           # Set timeout to handle long-running requests
