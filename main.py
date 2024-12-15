c8de9df5cc774f158f93110d643cbbed0da6ef4f
james-fairclough <j_fairclough@ymail.com>
2024-12-15 15:41:21 +1100

    Removing local run

--- main.py
+++ main.py
@@ -196,5 +196,5 @@ def updatePlayerStats():
   return "Player's stats updated successfully!", 200   
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+#if __name__ == "__main__":
+    #app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
